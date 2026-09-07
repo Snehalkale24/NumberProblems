82dd1ad8dd2fa4a9483d3154c118a3ceeb7f6d91
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-07 18:14:06 +0530

    add

---
+++ sum_digit.java
@@ -0,0 +1,18 @@
+public class sum_digit{
+	public static void sum(int no)
+	{
+		int sum=0;
+		while(no>0)
+		{
+		int nno=no%10;
+		sum=sum+nno;
+		no=no/10;
+	}
+	System.out.println(sum);
+}
+public static void main(String args[])
+{
+	sum(12345);
+}
+
+}
