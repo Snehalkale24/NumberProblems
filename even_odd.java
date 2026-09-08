747f1cb6036dd77dba97a9b0eb1a0eef79ee156a
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-08 21:50:39 +0530

    add

--- even_odd.java
+++ even_odd.java
@@ -1,17 +1,34 @@
-import java.util.*;
+// import java.util.*;
+// public class even_odd{
+// 	public static void even(int no)
+// 	{
+// 			if(no%2==0)
+// 			{
+// 				System.out.println("even"+no);
+// 			}
+// 			else{
+// 				System.out.println("odd"+no);
+// 			}
+// 		}
+// 	public static void main(String args[])
+// 	{
+// 		even(12);
+// 	}
+// }
+
 public class even_odd{
 	public static void even(int no)
 	{
-			if(no%2==0)
-			{
-				System.out.println("even"+no);
-			}
-			else{
-				System.out.println("odd"+no);
-			}
-		}
+	  if(no%2==0)
+	  {
+	  	System.out.println("even no"+no);
+	  }
+	  else{
+	  	System.out.println("odd"+no);
+	  }
+	}
 	public static void main(String args[])
 	{
-		even(12);
+		even(13);
 	}
 }
