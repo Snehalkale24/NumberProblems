99917b1519109728fb08e41924f53dcaf7025cc4
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-08 20:33:27 +0530

    add

---
+++ count_no.java
@@ -0,0 +1,17 @@
+// public class count_no
+// {
+// 	public static void count(int no)
+// 	{
+// 		int count=0;
+// 		while(no>0)
+// 		{
+//             no=no/10;
+//             count++;
+// 		}
+// 		System.out.println("no of digit:"+count);
+// 	}
+// 	public static void main(String args[])
+// 	{
+// 		count(12345);
+// 	}
+// }
