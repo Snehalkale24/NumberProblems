4c8acefacecb8226eda7460cd7132f17f8617cd1
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-10 21:33:00 +0530

    add

--- first_digit.java
+++ first_digit.java
@@ -14,22 +14,18 @@
 
 
 
-public class first_digit {
-    public static void first(int no) {
-        while(no >= 10) {
-            no = no / 10;
+public class first_digit{
+    public static void main(String args[])
+    {
+        int no[]=12345;
+        int rem=0;
+        while(no>0)
+        {
+            rem=no/10;
         }
-
-        System.out.println(no);
-    }
-
-    public static void main(String args[]) {
-        first(1234);
+        System.out.print(rem);
     }
 }
-
-
-
 
 
 
