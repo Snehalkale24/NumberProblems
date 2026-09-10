069d9e11168dc916a0dcc3ae09ee7807a97248b0
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-10 21:37:25 +0530

    add

---
+++ largest_two_no.java
@@ -0,0 +1,44 @@
+// public class largest_two_no
+// {
+// 	public static void main(String args[])
+// 	{
+// 		int a=34;
+// 		int b=55;
+// 		if(a>b)
+// 		{
+// 			System.out.println("a is largest"+a);
+// 		}
+// 		else if(a<b)
+// 		{
+// 			System.out.println("b is largest"+b);
+// 		}
+// 		else{
+// 			System.out.println("both no are equal");
+// 		}
+// 	}
+// }
+
+
+
+
+
+
+
+
+public class largest_two_no{
+	public static void main(String args[])
+	{
+		int a=12;
+		int b=44;
+	    if(a>b)
+	    {
+	    	System.out.println("a is larger"+a);
+	    }
+	    else if(b>a){
+	    	System.out.println("b is larger:"+b);
+	    }
+	    else{
+	    	System.out.println("both no are equals");
+	    }
+	}
+}
