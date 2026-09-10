4de8fdc9f44cff9f454f7978fabf921145a422bd
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-10 21:44:20 +0530

    add

--- product.java
+++ product.java
@@ -1,15 +1,15 @@
-public class product
-{
-	public static void main(String args[])
-{
-	int no=123345;
-	int prod=1;
-	while(no>0)
-	{
-		int mul=no%10;
-		prod=prod*mul;
-		no=no/10;
-	}
-	System.out.println("product of digit"+prod);
-}
-}
+// public class product
+// {
+// 	public static void main(String args[])
+// {
+// 	int no=123345;
+// 	int prod=1;
+// 	while(no>0)
+// 	{
+// 		int mul=no%10;
+// 		prod=prod*mul;
+// 		no=no/10;
+// 	}
+// 	System.out.println("product of digit"+prod);
+// }
+// }
