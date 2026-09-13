85b446ff90ffdf556175dca408e59be64cd4e9b8
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-13 23:55:24 +0530

    add

---
+++ swap_two_no.java
@@ -0,0 +1,15 @@
+public class swap_two_no
+{
+	public static void swap(int a,int b)
+	{
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("a"+a);
+		System.out.println("b"+b);
+	}
+	public static void main(String args[])
+	{
+		swap(12,33);
+	}
+};
