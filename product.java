0856a19beb4e16ed2f2dc51f942f459c2c8f6e06
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-13 23:59:17 +0530

    add

--- product.java
+++ product.java
@@ -15,20 +15,3 @@
 // }
 
 
-
-public class product{
-	public static void prod(int no)
-	{
-		int product=1;
-		while(no>0){
-			int digit=no%10;
-			product=product*digit;
-			no=no/10;
-		}
-		System.out.println(product);
-	}
-	public static void main(String args[])
-	{
-		prod(1234);
-	}
-}
