1616109d6c238c0787e02d1ac84dafa94ea7e572
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-20 00:04:51 +0530

    add

--- square.java
+++ square.java
@@ -13,16 +13,13 @@
 // }
 
 
-
 import java.util.*;
-public class square{
-	public static void square(int no)
-	{
-		int square=no*no;
-		System.out.println("square:"+square);
-	}
-	public static void main(String args[])
-	{
-		square(2);
-	}
+public static void square(int no)
+{
+	int square=no*no;
+	System.out.println("square:"+square);
+}
+public static void main(String args[])
+{
+	square(2);
 }
