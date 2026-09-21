09b845003056d09f09253051c125f98fcb448b4b
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-22 01:08:00 +0530

    add

--- sumofodd.java
+++ sumofodd.java
@@ -1,30 +1,15 @@
-import java.util.*;
-public class sumofodd
-{
+public class sumofodd{
 	public static void main(String args[])
 	{
-		Scanner s = new Scanner(System.in);
-
-        System.out.print("Enter number: ");
-        int a=s.nextInt();
-        System.out.println("enter second no:");
-        int b=s.nextInt();
-        System.out.println("enter third no:");
-        int c=s.nextInt();
-
-        if(a<=b && a<=c)
-        {
-        	System.out.println("smallest:"+a);
-        }
-        else if(b<=a && b<=c)
-        {
-        	System.out.println("smallest:"+b);
-        }
-        else{
-        	System.out.println("smallest:"+c);
-        }
-        
-      
-     
+		int n=10;
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2!=0)
+			{
+				sum=sum+i;
+			}
+		}
+		System.out.println("odd:"+sum);
 	}
 }
