5c32df1202b7747c9821ff27bdede34a8084fc47
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-22 14:03:40 +0530

    add

--- NaturalNoReverse.java
+++ NaturalNoReverse.java
@@ -1,13 +1,27 @@
+// import java.util.*;
+// public class NaturalNoReverse{
+// 	public static void main(String args[])
+// 	{
+// 		Scanner s=new Scanner(System.in);
+// 		System.out.println("enter no:");
+// 		int n=s.nextInt();
+// 		for(int i=n;i>=1;i--)
+// 		{
+// 			System.out.println(i);
+// 		}
+// 	}
+// }
+
 import java.util.*;
 public class NaturalNoReverse{
 	public static void main(String args[])
 	{
-		Scanner s=new Scanner(System.in);
-		System.out.println("enter no:");
-		int n=s.nextInt();
-		for(int i=n;i>=1;i--)
+		int n=12;
+		while(n>=1)
 		{
-			System.out.println(i);
+			System.out.println(n);
+			n--;
 		}
+		System.out.println(n);
 	}
 }
