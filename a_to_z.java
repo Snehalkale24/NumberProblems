0d672797f4da29dff97ca96ff9a44aa1ba08f131
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-22 14:20:26 +0530

    add

---
+++ a_to_z.java
@@ -0,0 +1,11 @@
+public class a_to_z{
+	public static void main(String args[])
+	{
+		char ch='a';
+		while(ch<='z')
+		{
+			System.out.println(ch);
+			ch++;
+		}
+	}
+}
---
+++ even_no.java
@@ -0,0 +1,14 @@
+public class even_no{
+	public static void main(String args[])
+	{
+		int n=2;
+		while(n<=10)
+		{
+          if(n%2==0)
+          {
+          	System.out.println(n);
+          }
+          n++;
+		}
+	}
+}
