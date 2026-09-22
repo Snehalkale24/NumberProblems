449359a7af559581204b62103f86d445d9845818
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-22 14:31:20 +0530

    add

--- even_no.java
+++ even_no.java
@@ -1,14 +1,27 @@
+// public class even_no{
+// 	public static void main(String args[])
+// 	{
+// 		int n=2;
+// 		while(n<=10)
+// 		{
+//           if(n%2==0)
+//           {
+//           	System.out.println(n);
+//           }
+//           n++;
+// 		}
+// 	}
+// }
+
 public class even_no{
 	public static void main(String args[])
 	{
 		int n=2;
-		while(n<=10)
+		for(int i=n;i<=10;i++)
 		{
-          if(n%2==0)
-          {
-          	System.out.println(n);
-          }
-          n++;
+			if(i%2==0){
+				System.out.println(i);
+			}
 		}
 	}
 }
